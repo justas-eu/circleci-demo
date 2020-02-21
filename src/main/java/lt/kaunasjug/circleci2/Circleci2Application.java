4b3e412aa17445fa2eb1750b5ec480b2df77015c
Justas Sperauskas <justas@justas.eu>
2020-02-21 13:39:43 +0200

    demo initial import

---
+++ src/main/java/lt/kaunasjug/circleci2/Circleci2Application.java
@@ -0,0 +1,13 @@
+package lt.kaunasjug.circleci2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Circleci2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Circleci2Application.class, args);
+	}
+
+}
---
+++ src/test/java/lt/kaunasjug/circleci2/Circleci2ApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.kaunasjug.circleci2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Circleci2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
